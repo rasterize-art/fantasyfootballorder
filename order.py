e049299e1e07c54dbd82e71e7b5371c81a40fefe
rasterize-art <73791278+rasterize-art@users.noreply.github.com>
2023-08-27 11:37:10 -0500

    Create order.py

---
+++ order.py
@@ -0,0 +1,21 @@
+# Importing the random library to shuffle the list
+import random
+
+# List of individuals in the fantasy football league
+participants = [
+    'Pranit',
+    'Yash',
+    'Anuj',
+    'bryce',
+    'Arjun',
+    'Deepti',
+    'Arnav',
+    'Praveen',
+    'Girish'
+]
+
+# Shuffling the list to get names in random order
+random.shuffle(participants)
+
+# Outputting the shuffled list
+participants
